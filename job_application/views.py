e84642d0d7fbcea0237f0e35e1e5d475fb50e166
MRBOX <olamigokeadebayo90@gmail.com>
2024-05-06 17:30:13 +0100

    Create a base template

--- job_application/views.py
+++ job_application/views.py
@@ -24,3 +24,7 @@ def index(request):
             messages.success(request, f"Dear {first_name}, your Form submitted successfully")
 
     return render(request, "index.html")
+
+
+def about(request):
+    return render(request, "about.html")
